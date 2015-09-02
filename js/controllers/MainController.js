2440d44b52a32a84cf7255c1a07035a745dc56f9
Satit Rianpit <satit@DevMate.local>
2015-09-02 11:21:53 +0700

    add stateParams

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,10 +2,19 @@
 
 angular.module('app.controllers.MainController', [])
     .controller('MainController', function ($scope) {
+
         $scope.products = [
             {id: 1, name: 'Angular'},
             {id: 2, name: 'Ionic Framework'},
             {id: 3, name: 'NodeJS'}
         ];
+
+        $scope.fruits = [
+            {name: 'Apple'},
+            {name: 'Orange'},
+            {name: 'Banana'}
+        ];
+
+        $scope.title = 'Hello';
     });
 
